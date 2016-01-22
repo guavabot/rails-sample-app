54a758f6904a4dc09e642c932fedad897c97d317
Ivan Soriano <ivansoriano@gmail.com>
2016-01-22 22:59:20 +0100

    set default page title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ivan's Sample App"
+    assert_select "title", "Ivan's Sample App"
   end
 
   test "should get help" do
