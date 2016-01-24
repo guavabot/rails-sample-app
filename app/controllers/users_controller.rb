0c1a697b6499a65bd46fec784a6f52b01b02811c
Ivan Soriano <ivansoriano@gmail.com>
2016-01-24 13:06:03 +0100

    flash on account creation success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = 'You are now part of the gang.'
       redirect_to @user
     else
       render 'new'
