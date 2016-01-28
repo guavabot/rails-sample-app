a2a94fbaa1e20d1d4e24e984818628d17f67a868
Ivan Soriano <ivansoriano@gmail.com>
2016-01-28 21:59:46 +0100

    implement friendly forwarding when edit while not logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -49,4 +49,16 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
+
+  # Redirects to stored location (or default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stored the location that was tried to access
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
 end
