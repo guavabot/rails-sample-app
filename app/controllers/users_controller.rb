ce2512a687f996886307d7df1315875b0c3b2302
Ivan Soriano <ivansoriano@gmail.com>
2016-02-08 21:50:29 +0100

    display tweets in user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tweets = @user.tweets.paginate(page: params[:page])
     redirect_to root_url unless @user.activated?
   end
 
