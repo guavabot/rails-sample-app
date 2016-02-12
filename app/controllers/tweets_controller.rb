fd12421b2e94c221caec66726a8242585e38b60c
Ivan Soriano <ivansoriano@gmail.com>
2016-02-12 21:06:14 +0100

    add destroy action for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: [:destroy]
 
   def create
     @tweet = current_user.tweets.build(tweet_params)
@@ -13,13 +14,20 @@ class TweetsController < ApplicationController
   end
 
   def destroy
-
+    @tweet.destroy
+    flash[:success] = 'Tweet deleted'
+    redirect_to request.referer || root_url
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:content)
+  end
+
+  def correct_user
+    @tweet = current_user.tweets.find_by(id: params[:id])
+    redirect_to root_url if @tweet.nil?
   end
 
 end
