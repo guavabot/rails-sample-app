735bbafe65dc8718c58d1b32b6a2dcff69ec3b61
Ivan Soriano <ivansoriano@gmail.com>
2016-04-13 23:37:34 +0200

    test picture in Tweets integration test

--- test/integration/tweets_interface_test.rb
+++ test/integration/tweets_interface_test.rb
@@ -10,6 +10,7 @@ class TweetsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
 
     #Invalid submission
     assert_no_difference 'Tweet.count' do
@@ -19,9 +20,11 @@ class TweetsInterfaceTest < ActionDispatch::IntegrationTest
 
     #Valid submission
     content = 'This post is awe-worthy'
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Tweet.count', 1 do
-      post tweets_path, tweet: { content: content }
+      post tweets_path, tweet: { content: content, picture: picture }
     end
+    assert @user.tweets.first.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -36,6 +39,21 @@ class TweetsInterfaceTest < ActionDispatch::IntegrationTest
     #Visit different user
     get user_path(users(:samuel))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test 'tweet sidebar count' do
+    log_in_as(@user)
+    get root_path
+    assert_match '34 tweets', response.body
+
+    # User with zero tweets
+    other_user = users(:lonely)
+    log_in_as(other_user)
+    get root_path
+    assert_match '0 tweets', response.body
+    other_user.tweets.create!(content: "A tweet")
+    get root_path
+    assert_match '1 tweet', response.body
   end
 
 end
