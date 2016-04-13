2e987022e5544e5f7b255ded426518e5c460efcc
Ivan Soriano <ivansoriano@gmail.com>
2016-04-13 21:56:13 +0200

    add CarrierWave for PictureUploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207130105) do
+ActiveRecord::Schema.define(version: 20160413195028) do
 
   create_table "tweets", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   add_index "tweets", ["user_id", "created_at"], name: "index_tweets_on_user_id_and_created_at"
