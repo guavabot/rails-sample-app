7142b366ddbe95b13281660eaee188e684c48c30
Ivan Soriano <ivansoriano@gmail.com>
2016-04-17 12:35:01 +0200

    display following/followers stats in home

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,11 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :tweets, only: [:create, :destroy]
